0e1a50030af3fccb35f6ba478c3a599c58a9f815
mile <chenyang20010703@outlook.com>
2020-04-21 22:44:24 +0800

    push

--- bai/plugins/weather/data_source.py
+++ bai/plugins/weather/data_source.py
@@ -1,2 +1,35 @@
+import json
+from urllib.parse import quote
+
+import requests
+from bs4 import BeautifulSoup
+
+moji_api_get_city_code_url = 'http://tianqi.moji.com/api/citysearch/'
+moji_api_get_weather_url = 'http://tianqi.moji.com/api/redirect/'
+
+
 async def get_weather_of_city(city: str) -> str:
-    return f'{city}的天气是……'
+    citycode = get_city_code_by_str(city)
+    if citycode is None:
+        return '呃，没有听说过这个地方🦢'
+    else:
+        return get_weather_from_moji(citycode)
+
+
+def get_city_code_by_str(city: str) -> int:
+    response = requests.get(moji_api_get_city_code_url + quote(city))
+    try:
+        return json.loads(response.text).get('city_list')[0].get('cityId')
+    except:
+        return None
+
+
+def get_weather_from_moji(city: int) -> str:
+    response = requests.get(moji_api_get_weather_url + str(city))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    temperature = soup.find(class_='wea_weather').find('em').get_text()
+    meteorological = soup.find(class_='wea_weather').find('b').get_text()
+    humidity = soup.find(class_='wea_about').find('span').get_text()
+    wind = soup.find(class_='wea_about').find('em').get_text()
+    tips = soup.find(class_='wea_tips').find('em').get_text()
+    return f'温度{temperature}℃，{meteorological}，{humidity}，{wind}，{tips}'
