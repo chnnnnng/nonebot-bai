cca2e843486d2ae3ea6a9df2525ad327fea8d33f
mile <chenyang20010703@outlook.com>
2020-04-23 22:55:48 +0800

    push

--- bai/plugins/webserver/__init__.py
+++ bai/plugins/webserver/__init__.py
@@ -8,7 +8,7 @@ bot = nonebot.get_bot()  # 在此之前必须已经 init
 @bot.server_app.route('/login')
 async def login():
     # await bot.send_private_msg(596552206, '你的主页被访问了')
-    return await render_template('login.html')
+    return await render_template('plugins/webserver/templates/login/login.html')
 
 
 #热更新借口，向此接口发送POST请求即可重启
