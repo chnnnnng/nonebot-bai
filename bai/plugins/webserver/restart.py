e7e8bafb2855c20ec4434ee74572cd5dcd5e7489
mile <chenyang20010703@outlook.com>
2020-04-23 21:51:07 +0800

    push

--- bai/plugins/webserver/restart.py
+++ bai/plugins/webserver/restart.py
@@ -1,18 +1,6 @@
 import os
 from os import path
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-
-def x():
-    sched = BlockingScheduler()
-    sched.add_job(xx, 'interval', seconds=6)
-    sched.start()
-
-
-def xx():
-    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
-
 
 if __name__ == '__main__':
-    x()
+    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
