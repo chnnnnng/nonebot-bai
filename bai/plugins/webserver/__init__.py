9e7fea7c8f896c6b286e6b7e89f4d1546577c6bf
mile <chenyang20010703@outlook.com>
2020-04-23 20:15:13 +0800

    push

--- bai/plugins/webserver/__init__.py
+++ bai/plugins/webserver/__init__.py
@@ -8,7 +8,7 @@ bot = nonebot.get_bot()  # 在此之前必须已经 init
 
 @bot.server_app.route('/admin')
 async def admin():
-    #await bot.send_private_msg(596552206, '你的主页被访问了')
+    # await bot.send_private_msg(596552206, '你的主页被访问了')
     return '欢迎来到管理页面'
 
 
