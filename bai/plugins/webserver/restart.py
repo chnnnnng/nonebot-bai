5f4099d22127e0c08bc60fea58fb4cc48816957a
mile <chenyang20010703@outlook.com>
2020-04-23 22:17:17 +0800

    push

--- bai/plugins/webserver/restart.py
+++ bai/plugins/webserver/restart.py
@@ -3,4 +3,4 @@ from os import path
 
 
 if __name__ == '__main__':
-    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
+    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))),  'update.sh'))
