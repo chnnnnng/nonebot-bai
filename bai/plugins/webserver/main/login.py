b1481b8f29cd544cfdb374da289dfab5380b7aa3
mile <chenyang20010703@outlook.com>
2020-04-24 00:25:56 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -1,3 +1,5 @@
+from os import path
+
 import jinja2
 import nonebot
 from jinja2 import PackageLoader,Environment
@@ -9,7 +11,7 @@ bot = nonebot.get_bot()  # 在此之前必须已经 init
 @bot.server_app.route('/login')
 async def login():
     # await bot.send_private_msg(596552206, '你的主页被访问了')
-    templateLoader = jinja2.FileSystemLoader(searchpath="../templates/")
+    templateLoader = jinja2.FileSystemLoader(searchpath=path.join(path.dirname(__file__),'templates'))
     templateEnv = jinja2.Environment(loader=templateLoader)
     TEMPLATE_FILE = "login.html"
     template = templateEnv.get_template(TEMPLATE_FILE)
