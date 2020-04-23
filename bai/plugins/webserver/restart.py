baa5ca9d82c71f95af75d27f1130bbe03e4e262c
mile <chenyang20010703@outlook.com>
2020-04-23 19:57:52 +0800

    push

--- bai/plugins/webserver/restart.py
+++ bai/plugins/webserver/restart.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     print("开始重启")
     # os.system('sleep 10s; print "REEEEEEEEEEEEEEEE"')
     # os.system('ps -aux | grep bot.py | grep -v \"grep\" | awk \'{print $2}\' | xargs kill -9')
-    # os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
+    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
     ##os.system('pkill python')
-    python = sys.executable
-    os.execl(python, 'python', path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'bot.py'))
+    #python = sys.executable
+    #os.execl(python, 'python', path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'bot.py'))
