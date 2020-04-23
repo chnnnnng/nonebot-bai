f1bb13cb2836ab1830a30f66612709178441723c
mile <chenyang20010703@outlook.com>
2020-04-23 17:33:25 +0800

    push

--- bai/plugins/webserver/__init__.py
+++ bai/plugins/webserver/__init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import nonebot
 
 bot = nonebot.get_bot()  # 在此之前必须已经 init
@@ -6,3 +9,12 @@ bot = nonebot.get_bot()  # 在此之前必须已经 init
 async def admin():
     #await bot.send_private_msg(596552206, '你的主页被访问了')
     return '欢迎来到管理页面'
+
+
+@bot.server_app.route('/update')
+async def update():
+    try:
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+    finally:
+        return 'OK'
