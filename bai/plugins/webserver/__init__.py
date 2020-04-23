cbbe7e67fca66d922ce178fc6830d0b2e3214141
mile <chenyang20010703@outlook.com>
2020-04-23 22:40:51 +0800

    push

--- bai/plugins/webserver/__init__.py
+++ bai/plugins/webserver/__init__.py
@@ -1,22 +1,17 @@
-import os
-from os import path
-
 import nonebot
 import requests
+from quart import render_template
 
 bot = nonebot.get_bot()  # 在此之前必须已经 init
 
 
-@bot.server_app.route('/')
-async def admin():
+@bot.server_app.route('/login')
+async def login():
     # await bot.send_private_msg(596552206, '你的主页被访问了')
-    return 'QBot管理页面'
+    return render_template('login.html')
 
 
-def restart():
-    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
-
-
+#热更新借口，向此接口发送POST请求即可重启
 @bot.server_app.route('/update', methods=['POST'])
 async def update():
     try:
