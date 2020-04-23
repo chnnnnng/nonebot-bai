22f28481805a8d47dd9a3e59dbb4d2ae16acce66
mile <chenyang20010703@outlook.com>
2020-04-23 23:59:27 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -7,4 +7,4 @@ bot = nonebot.get_bot()  # 在此之前必须已经 init
 @bot.server_app.route('/login')
 async def login():
     # await bot.send_private_msg(596552206, '你的主页被访问了')
-    return await render_template('login/login.html')
+    return await render_template('login.html')
