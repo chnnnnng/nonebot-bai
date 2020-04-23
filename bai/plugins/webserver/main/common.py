22a1d1e9053ca6eb34f2f0e69361f2152376f613
mile <chenyang20010703@outlook.com>
2020-04-24 00:46:45 +0800

    push

--- bai/plugins/webserver/main/common.py
+++ bai/plugins/webserver/main/common.py
@@ -3,7 +3,7 @@ from os import path
 import jinja2
 
 
-async def getTemplate(TEMPLATE_FILE):
+def getTemplate(TEMPLATE_FILE):
     templateLoader = jinja2.FileSystemLoader(searchpath=path.join(path.dirname(path.dirname(__file__)),'templates',TEMPLATE_FILE))
     templateEnv = jinja2.Environment(loader=templateLoader)
-    return await templateEnv.get_template(TEMPLATE_FILE+'.html')
+    return templateEnv.get_template(TEMPLATE_FILE+'.html')
