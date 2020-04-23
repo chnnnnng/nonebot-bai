8ebf8c08146673100299643b28a0e05c52504d9f
mile <chenyang20010703@outlook.com>
2020-04-24 00:18:33 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -3,11 +3,11 @@ from jinja2 import PackageLoader,Environment
 from quart import render_template
 
 bot = nonebot.get_bot()  # 在此之前必须已经 init
-env = Environment(loader=PackageLoader('webserver','templates'))
 
 
 @bot.server_app.route('/login')
 async def login():
     # await bot.send_private_msg(596552206, '你的主页被访问了')
-    template = env.get_template('login/login.html')  # 获取一个模板文件
+    env = Environment(loader=PackageLoader("webserver", "templates"))
+    template = env.get_template('login.html')
     return await template.render()  # 渲染
