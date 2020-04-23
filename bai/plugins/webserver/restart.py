304eae556aa7ec1d3121d3f6dbdc732e5289e821
mile <chenyang20010703@outlook.com>
2020-04-23 19:39:07 +0800

    push

--- bai/plugins/webserver/restart.py
+++ bai/plugins/webserver/restart.py
@@ -2,13 +2,14 @@ import os
 import sys
 import time
 from os import path
+import asyncio
 
 if __name__ == '__main__':
     time.sleep(2)
     print("开始重启")
     #os.system('sleep 10s; print "REEEEEEEEEEEEEEEE"')
     #os.system('ps -aux | grep bot.py | grep -v \"grep\" | awk \'{print $2}\' | xargs kill -9')
-    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'ms.sh'))
-    os.system('pkill python')
+    os.system('sh ' + path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'update.sh'))
+    ##os.system('pkill python')
     #python = sys.executable
     #os.execl(python, 'python', path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'bot.py'))
