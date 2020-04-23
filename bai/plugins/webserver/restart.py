a5bbe913d861b86fca14554a9e183c64a154efdd
mile <chenyang20010703@outlook.com>
2020-04-23 18:46:20 +0800

    push

--- bai/plugins/webserver/restart.py
+++ bai/plugins/webserver/restart.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
 from os import path
 
 if __name__ == '__main__':
     print("开始重启")
+    time.sleep(2)
     python = sys.executable
-    #os.execl(python, python, path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'bot.py'))
+    os.execl(python, python, path.join(path.dirname(path.dirname(path.dirname(path.dirname(__file__)))), 'bot.py'))
