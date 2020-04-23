ad1c7743902d7f84dd3d87b55c1b8e72fdf781e8
mile <chenyang20010703@outlook.com>
2020-04-23 23:17:41 +0800

    push

--- bai/plugins/webserver/__init__.py
+++ bai/plugins/webserver/__init__.py
@@ -1,14 +1,7 @@
 import nonebot
 import requests
-from quart import render_template
 
 bot = nonebot.get_bot()  # 在此之前必须已经 init
-
-
-@bot.server_app.route('/login')
-async def login():
-    # await bot.send_private_msg(596552206, '你的主页被访问了')
-    return await render_template('login.html')
 
 
 #热更新借口，向此接口发送POST请求即可重启
@@ -17,5 +10,5 @@ async def update():
     try:
         return 'OK'
     finally:
-        requests.post("http://127.0.0.1:94/update")
+        requests.post("http://127.0.0.1:94/update")#向本机94端口的一个小服务发送post
 
---
+++ bai/plugins/webserver/main/login.py
@@ -0,0 +1,10 @@
+import nonebot
+from quart import render_template
+
+bot = nonebot.get_bot()  # 在此之前必须已经 init
+
+
+@bot.server_app.route('/login')
+async def login():
+    # await bot.send_private_msg(596552206, '你的主页被访问了')
+    return await render_template('login/login.html')
