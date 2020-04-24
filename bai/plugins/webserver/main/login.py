e0a375b3ad4994e4866810e1a34ddca8bb2f8361
mile <chenyang20010703@outlook.com>
2020-04-24 21:23:16 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -1,10 +1,15 @@
 import nonebot
+from quart import request
+
 from . import common
 bot = nonebot.get_bot()  # 在此之前必须已经 init
 
 
-@bot.server_app.route('/login')
+@bot.server_app.route('/login', methods=['GET','POST'])
 async def login():
-    # await bot.send_private_msg(596552206, '你的主页被访问了')
-    template = common.getTemplate('login')
-    return template.render()  # 渲染
+    if request.method == 'GET':
+        # await bot.send_private_msg(596552206, '你的主页被访问了')
+        template = common.getTemplate('login')
+        return template.render()  # 渲染
+    else:
+        await bot.send_private_msg(596552206, '你的主页被访问了')
