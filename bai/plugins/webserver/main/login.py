671ac39d4c6672e55f8667899c55883baa8dfe46
mile <chenyang20010703@outlook.com>
2020-04-24 22:37:10 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -15,8 +15,8 @@ async def login():
         template = common.getTemplate('login')
         return template.render()  # 渲染
     else:
-        if await request.form.get('action') == 'get':
-            user = await request.form.get('qq')
+        if (await request.form).get('action') == 'get':
+            user = (await request.form).get('qq')
             now = datetime.now(pytz.timezone('Asia/Shanghai'))
             vc = str(int(user[0:5])*int(now.minute)*int(now.hour)*int(now.day)+1000)[0:5]
             await bot.send_private_msg(user_id=596552206, message=f'【{vc}】是您的验证码，该验证码一分钟有效。您的账号正在尝试登陆管理页面，如非本人操作，请防范有关风险。')
