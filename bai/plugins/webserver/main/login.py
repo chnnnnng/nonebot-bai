aabdad539406e2f33a9b2efe3df715288cf31cd8
mile <chenyang20010703@outlook.com>
2020-04-24 23:37:56 +0800

    push

--- bai/plugins/webserver/main/login.py
+++ bai/plugins/webserver/main/login.py
@@ -1,17 +1,27 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import nonebot
 import pytz
-from quart import request, redirect, url_for
+from quart import request, redirect, url_for, session
 
 from . import common
 bot = nonebot.get_bot()  # 在此之前必须已经 init
+bot.server_app.config['SECRET_KEY'] = 'cxb'
+bot.server_app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
+
+@bot.server_app.route('/', methods=['GET'])
+async def login():
+    if session['adminqq'] is not None:
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('login'))
 
 
 @bot.server_app.route('/login', methods=['GET','POST'])
 async def login():
     if request.method == 'GET':
-        # await bot.send_private_msg(596552206, '你的主页被访问了')
+        if session['adminqq'] is not None:
+            return redirect(url_for('admin'))
         template = common.getTemplate('login')
         return template.render()  # 渲染
     else:
@@ -27,11 +37,16 @@ async def login():
             vc = str(int(user[0:5]) * int(now.minute) * int(now.hour) * int(now.day) + 1000)[1:5]
             vc_input = (await request.form).get('vc')
             if vc == vc_input:
+                session['adminqq'] = user
+                session.permanent = True
                 return redirect(url_for('admin'))
             else:
-                return "ACCESS DENIED."
+                return redirect(url_for('login'))
 
 
 @bot.server_app.route('/admin', methods=['GET'])
 async def admin():
-    return "THIS IS ADMIN PAGE"
+    if session['adminqq'] is None:
+        return redirect(url_for('login'))
+    else:
+        return "THIS IS ADMIN PAGE"
