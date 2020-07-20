f76ef1e9f781f4b85496ed1a8015f55bcc8a31c1
mile <chenyang20010703@outlook.com>
2020-07-20 23:05:23 +0800

    push

--- bai/plugins/healthreport/__init__.py
+++ bai/plugins/healthreport/__init__.py
@@ -13,7 +13,7 @@ hr_header = {'Cookie':'JSESSIONID=0B8741E0E4E237CB714CA05C1D470AE7','Content-Typ
 
 @on_command('healthreport', aliases=('健康上报', '打卡'))
 async def healthreport(session: CommandSession):
-    await session.send("好的，稍等")
+    await session.send("好的，稍等。")
     sleep(3)
     await session.send(dopost())
 
