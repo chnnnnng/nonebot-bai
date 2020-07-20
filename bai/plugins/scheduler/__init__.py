ca9d53909d0cbb93ac222f892083e5188c121677
mile <chenyang20010703@outlook.com>
2020-07-20 22:29:46 +0800

    push

--- bai/plugins/scheduler/__init__.py
+++ bai/plugins/scheduler/__init__.py
@@ -6,10 +6,9 @@ from aiocqhttp.exceptions import Error as CQHttpError
 
 
 #早上提醒打卡
-@nonebot.scheduler.scheduled_job('cron', hour=22, minute=26)
+@nonebot.scheduler.scheduled_job('cron', hour=22, minute=30)
 async def _():
     bot = nonebot.get_bot()
-    now = datetime.now(pytz.timezone('Asia/Shanghai'))
     try:
         await bot.send_private_msg(user_id=596552206, message='起床啦！记得健康上报')
         await bot.send_private_msg(user_id=596552206, message='回复:"健康上报"或"打卡",我会紫东为您打卡')
